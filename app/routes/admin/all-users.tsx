281385a4c5aca8c18771e51028deb9270d313b14
Prashrijan <shresthaprashrijan@gmail.com>
2025-06-25 23:33:11 +1000

    layout setup

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,16 @@
+import { Header } from "components";
 import React from "react";
 
 const AllUsers = () => {
-    return <div>AllUsers</div>;
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title={"Trips Page"}
+                description="Check out our current users in real time"
+            />
+            All Users page content
+        </main>
+    );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Header } from "components";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const user = {
+        name: "Prashrijan",
+        email: "shresthaprashrijan@gmail.com",
+        imageUrl: "/assets/images/david.webp",
+    };
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title={`Welcome ${user?.name ?? "Guest"} 👋`}
+                description="Track activity, trends and popular destinations in real time."
+            />
+            dashboard page contents
+        </main>
+    );
 };
 
 export default Dashboard;
